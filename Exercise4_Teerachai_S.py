3ab898ea8890d855e9d97935dab570824ca2e371
Mr.Thirachai Sueapo <mandmod@gmail.com>
2020-10-06 16:47:51 +0700

    Home Work

---
+++ Exercise4_Teerachai_S.py
@@ -0,0 +1,9 @@
+SFE = 70
+SGB = 75
+SICS = 90
+SCP = 95
+print('--------------Your Score--------------')
+print('Foundation English              : ',SFE)
+print('General Bussiness               : ',SGB)
+print('Introduction to Computer System : ',SICS)
+print('Computer Programing             : ',SCP)
