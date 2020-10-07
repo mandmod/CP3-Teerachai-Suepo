febd4555da07722e030c284602573e5ed46dbe8c
Mr.Thirachai Sueapo <mandmod@gmail.com>
2020-10-07 13:26:24 +0700

    First Create File and First Commit

---
+++ lecture25-Operation.py
@@ -0,0 +1,14 @@
+x = 10
+y = 5
+z = x + y
+print(z)
+print()
+
+money = 150
+incomePerDay = 300
+costPerDay = 120
+result = 150 + 300 * 30 - 120 * 30
+print(result)  # todo คำตอบที่ได้ผิด
+print()
+result = ((150 + 300) * 30) - (120 * 30)
+print(result)  # todo คำตอบที่ได้ผิด
---
+++ lecture29-AssignmentOperator.py
@@ -0,0 +1,19 @@
+a = 10
+b = 5
+c = a + b
+print('--------------------------')
+a = 10
+b = 12
+c = a + b
+a += 5  # todo a=10 + a=5 =15
+print(a)
+print('--------------------------')
+money = 1000
+money = money - 50
+money = money - 20
+print(money)
+print('--------------------------')
+money = 1000
+money -= 50
+money -= 20
+print(money)
