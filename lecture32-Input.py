d4568d5124df22575c8ab3cd6654c235c18bc84f
Mr.Thirachai Sueapo <mandmod@gmail.com>
2020-10-07 13:48:13 +0700

    First Create File and First Commit

---
+++ lecture32-Input.py
@@ -0,0 +1,17 @@
+'''
+x = input()
+print(type(x))
+print('Hello', x)
+'''
+'''
+print('--------------------------')
+name = input('กรุณากรอกชื่อ : ')
+lastName = input('กรุณากรอกนามสกุล : ')
+print('Hello', name, lastName)
+print('--------------------------')
+'''
+print('<----- Program Sum ----->')
+a = input('Input Number1 : ')
+b = input('Input Number2 : ')
+s = int(a) + int(b)
+print('Results is :', a, '+', b, '=', s)
