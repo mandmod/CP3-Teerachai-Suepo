0005d013dd99f182a255d59f664664ca458a4677
Mr.Thirachai Sueapo <mandmod@gmail.com>
2020-10-07 14:34:18 +0700

    First Create File and First Commit

---
+++ Exercise5_1_Teerachai_S.py
@@ -0,0 +1,10 @@
+print('<----- Program Cal. + - * / ----->')
+# num1, num2 = 10, 2
+# todo อ่านโจทย์ไม่แตก  ให้ผู้ใช้งานแก้ไข Code น้อยที่สุด ก็ต้อง Input ค่าเข้าไป ไม่ต้องแก้ Code เลย
+
+num1 = float(input('กรุณากรอกตัวเลข ตัวที่1 : '))
+num2 = float(input('กรุณากรอกตัวเลข ตัวที่2 : '))
+print(num1, '+', num2, '=', num1 + num2)
+print(num1, '-', num2, '=', num1 - num2)
+print(num1, '*', num2, '=', num1 * num2)
+print(num1, '/', num2, '=', num1 / num2)
---
+++ Exercise5_2_Teerachai_S.py
@@ -0,0 +1,8 @@
+print("<----- Velocity Calculation Program ----->")
+print()
+distance = int(input("Please insert Distance : "))
+time = int(input("Please insert time : "))
+print()
+print("Velocity =",distance/time, "(km/hr)")
+print()
+print("<---------------------------------------->")
